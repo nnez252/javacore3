8dca9b3315f631698e8415c3e7054f148166b508
Егор <nnez252@gmail.com>
2023-01-02 15:21:06 +0200

    zaadanie 3

---
+++ src/Bus.java
@@ -0,0 +1,6 @@
+public class Bus extends Transport{
+    public Bus(String brand, String model, int year, String contry, String color) {
+        super(brand, model, year, contry, color);
+    }
+
+}
